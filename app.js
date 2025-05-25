f1d63b96309effe4f13c759f1bc231338a3caa7b
EshaShah <2004.esha.shah@gmail.com>
2025-05-25 10:44:47 +0530

    added signup and login

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("connect-flash");
 
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+
+const user = require("./models/user.js");
+
 const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -19,8 +24,9 @@ app.use(methodOverride("_method"));
 
 const ExpressError = require("./utils/ExpressError.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/HopNStay";
 
@@ -52,14 +58,32 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(user.authenticate()));
+
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+// app.get("/demouser", async (req, res) => {
+//     let fakeUser = new user({
+//         email: "student@gmail.com",
+//         username: "student"
+//     });
+
+//     let registeredUser = await user.register(fakeUser, "helloworld");
+//     res.send(registeredUser);
+// });
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 app.all(/.*/, (req, res, next) => {
     next(new ExpressError(404, "Page Not Found"));
---
+++ routes/user.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+const user = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+
+router.get("/signup", (req, res) => {
+    res.render("users/signup.ejs");
+});
+
+router.post("/signup", wrapAsync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new user({ email, username });
+        const registeredUser = await user.register(newUser, password);
+        console.log(registeredUser);
+        req.flash("success", "Welcome to HopNStay");
+        res.redirect("/listings");
+    } catch (e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+}));
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+router.post("/login", passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), async (req, res) => {
+    req.flash("success", "Welcome back to HopNStay! You are logged in!");
+    res.redirect("/listings");
+});
+
+module.exports = router;
