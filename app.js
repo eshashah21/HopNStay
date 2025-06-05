42efbb00b6577d11d01b3b92e090cb9ab3c58a3f
EshaShah <2004.esha.shah@gmail.com>
2025-06-05 22:29:57 +0530

    Improve map responsiveness for small screens

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.all(/.*/, (req, res, next) => {
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong" } = err;
     // res.render("error.ejs", {err});
-    res.status(statusCode).render("error.ejs", { message });
+    res.status(statusCode).render("error", { message });
     // res.status(statusCode).send(message);
 });
 
